9a1f4e1e19eea229da620353cc40ddc9727f575c
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2022-10-08 22:01:09 -0600

    Add files via upload

---
+++ CALculator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main(string [] args)
+        {
+            int result = 0;
+            
+            Console.WriteLine("Enter first num");
+            string firstNum = Console.ReadLine();
+            int firstNumInt = int.Parse(firstNum);
+            Console.WriteLine("First number is " + firstNumInt);
+
+            Console.WriteLine("Please enter your operator");
+            string oper = Console.ReadLine();
+            Console.WriteLine("Your operator is:"+ oper);
+
+            Console.WriteLine("Enter second num");
+            string secondNum = Console.ReadLine();
+            int secondNumInt = int.Parse(secondNum);
+            Console.WriteLine("Second number is " + secondNumInt);
+           
+            
+            
+            if (oper=="+")
+            {
+                result = firstNumInt + secondNumInt;
+            }
+            else if (oper=="-")
+            {
+                result = firstNumInt - secondNumInt;
+            }
+            else if (oper == "*")
+            {
+                result = firstNumInt * secondNumInt;
+            }
+            else if(oper == "/")
+            {
+                result = firstNumInt / secondNumInt;
+            }
+            else {
+                Console.WriteLine("ebanant");
+                
+                
+            }
+
+            Console.WriteLine("Your result is: " + result);
+
+
+
+        }
+    }
+}
